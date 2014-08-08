58eab7ad6b21ec7482d8f34a05612b91c990d665
Kang-in Raon Kim <is.raonism@gmail.com>
2014-08-09 02:08:35 +0900

    add router for feedbacks.

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,13 +3,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from .views import TutorialViewSet, DocumentViewSet, BubbleViewSet, SpeechViewSet, NestedTutorialViewSet
+from .views import TutorialViewSet, FeedbackViewSet, DocumentViewSet, BubbleViewSet, SpeechViewSet, NestedTutorialViewSet
 
 admin.autodiscover()
 
 router = DefaultRouter()
 router.register(r'nested-tutorials', NestedTutorialViewSet)
 router.register(r'tutorials', TutorialViewSet)
+router.register(r'feedbacks', FeedbackViewSet)
 router.register(r'documents', DocumentViewSet)
 router.register(r'bubbles', BubbleViewSet)
 router.register(r'speechs', SpeechViewSet)
