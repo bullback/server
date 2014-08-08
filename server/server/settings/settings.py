72c6b7e4f191b8bc1c6ff816a223d173a83420df
Kang-in Raon Kim <is.raonism@gmail.com>
2014-08-08 19:10:25 +0900

    change db backends from sqlite3 to mysql.

--- server/server/settings/settings.py
+++ server/server/settings/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'server',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
