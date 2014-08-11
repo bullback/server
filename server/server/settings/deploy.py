dc9a2da7b851fbb610f7fc935f7f081931bdf2e0
Kang-in Raon Kim <is.raonism@gmail.com>
2014-08-12 06:51:58 +0900

    settings for Team Bullback server.

---
+++ server/server/settings/deploy.py
@@ -0,0 +1,15 @@
+from settings import *
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apiserver',
+        'USER': 'apiserver',
+        'PASSWORD': 'xlaqnfqor!!',
+        'HOST': '',
+        'PORT': '',
+    }
+}
